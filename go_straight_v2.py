30d4e5b354f52887c7590e34c94324dc4192e01f
ekoyuncal <62388510+ekoyuncal@users.noreply.github.com>
2023-04-18 02:46:17 +0300

    turtlebot3_basic_uniaxial motion_with_defined_goal

---
+++ go_straight_v2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+
+class goGoal():
+    def __init__(self):
+        rospy.init_node("go_goal")
+        self.goal_location = 5.0
+        self.current_location = 0.0
+        self.control = True
+
+        rospy.Subscriber("/odom", Odometry, self.odom_callback)
+        pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+        vel_msg = Twist()
+        rate = rospy.Rate(10)#10 time in 1 second
+        
+        while not rospy.is_shutdown():
+            
+            if self.control:
+                vel_msg.linear.x = 0.5
+                pub.publish(vel_msg)
+            else:
+                vel_msg.linear.x = 0.0
+                pub.publish(vel_msg)
+                rospy.loginfo("Goal Reached")
+            rate.sleep()
+
+
+    def odom_callback(self, msg):
+        #print(msg.pose.pose.position.x)
+        #pass
+        self.current_location = msg.pose.pose.position.x
+
+        if self.current_location < self.goal_location:
+            self.control = True
+        else:
+            self.control = False
+
+        
+try:
+    goGoal()
+except rospy.ROSInterruptException:
+    print("Ros Node is Done")
